fb71edaf1b709a26df4456d4408ab3d23cb46d7b
Tim M Schendzielorz <tim.schendzielorz@googlemail.com>
2026-03-07 11:17:47 +0100

    udpated hardware reservations

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,26 +11,26 @@ variable "nodes" {
     "pi-brain" = {
       local_ip  = "192.168.1.10" # Change to your Pi's Local IP
       is_master = true
-      cpu       = 4
+      cpu       = 2
       ram_gb    = 4
       res_cpu   = "200m"
-      res_ram   = "400Mi"
+      res_ram   = "500Mi"
     },
     "mac-worker" = {
       local_ip  = "127.0.0.1"
       is_master = false
-      cpu       = 2
+      cpu       = 4
       ram_gb    = 6
-      res_cpu   = "200m"
-      res_ram   = "800Mi"
+      res_cpu   = "4"
+      res_ram   = "8000Mi"
     },
     "ubuntu-worker" = {
       local_ip  = "192.168.1.12" # Change to your PC's Local IP
       is_master = false
       cpu       = 2
-      ram_gb    = 3
-      res_cpu   = "200m"
-      res_ram   = "512Mi"
+      ram_gb    = 4
+      res_cpu   = "4"
+      res_ram   = "8000Mi"
     }
   }
 }
